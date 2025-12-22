9f4c082f58616175af06856c2f18ed0686bf0bf6
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2025-12-22 17:52:06 +0100

    ci: fix versioning script to use github output

--- build/version/New-ReleaseVersion.ps1
+++ build/version/New-ReleaseVersion.ps1
@@ -1,15 +1,15 @@
 #!/usr/bin/env pwsh
 
-function New-ReleaseVersion {
-  [CmdletBinding()]
-  param ()
+[CmdletBinding()]
+param ()
 
-  [version]$CurrentDate = [version]::new((Get-Date -Format yy), (Get-Date).DayOfYear, 0)
-  [version]$CurrentVersion = (gh release list --limit 1 --json tagName --jq '.[0].tagName')
+[version]$CurrentDate = [version]::new((Get-Date -Format yy), (Get-Date).DayOfYear, 0)
+[version]$CurrentVersion = (gh release list --limit 1 --json tagName --jq '.[0].tagName')
 
-  if ($CurrentVersion -ge $CurrentDate) {
-    [version]::new($CurrentVersion.Major, $CurrentVersion.Minor, $CurrentVersion.Build + 1)
-  } else { 
-    $CurrentDate
-  }
+$TargetVersion = if ($CurrentVersion -ge $CurrentDate) {
+  [version]::new($CurrentVersion.Major, $CurrentVersion.Minor, $CurrentVersion.Build + 1)
+} else { 
+  $CurrentDate
 }
+
+"version=${TargetVersion}" >> $env:GITHUB_OUTPUT
